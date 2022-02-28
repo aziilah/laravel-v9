23ad4173a56c43165abfb5791c05598821d2a7ae
Aziilah <nuraziilahawg@gmail.com>
2022-03-01 00:00:34 +0800

    migration Post

--- database/migrations/2022_02_28_155021_create_posts_table.php
+++ database/migrations/2022_02_28_155021_create_posts_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class);  //FK
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
