481796736592c10238bf18e3190283514af4ac9b
Aziilah <nuraziilahawg@gmail.com>
2022-03-01 00:05:08 +0800

    factories Post

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,7 +17,9 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id' => mt_rand(1, 10),
+            'title' => $this->faker->sentence(),
+            'body' => $this->faker->paragraph()
         ];
     }
 }
